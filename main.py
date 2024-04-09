ab603c1241517e0666ed3e3c196b298f9f364d76
willnagoma <willnagoma@gmail.com>
2024-04-08 20:00:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ def load_csv(csv_file):
 stops=load_csv('Data/Officer_Traffic_Stops.csv')
 st.dataframe(stops)
 
-## Box plot
-box = alt.Chart(stops).mark_boxplot().encode(
-    x = alt.X('Was_a_Search_Conducted'),
-    y = alt.Y('Driver_Age')
-).properties(
-    width = 500,
-    title = 'Boxplot between Search Conducted vs Driver Age')
+
+@st.cache_data
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+stops = load_data('data/Officer_Traffic_Stops.csv')
